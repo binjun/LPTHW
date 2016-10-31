3a39d185e3d0525cf8a63d3b0fd9ec22d1c92a7a
binjun <binjun.wang@gmail.com>
2016-10-31 20:32:26 +0800

    add ex1

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+print "Hello world!"
+print u"你好，世界！"
+print "i like typing this."
+print "This is fun."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
