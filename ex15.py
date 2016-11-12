4f326102e52cad783731a6367f4446c4d501b13c
binjun <binjun.wang@gmail.com>
2016-11-12 18:38:18 +0800

    add new excises

---
+++ ex15.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+text_again = open(file_again)
+
+print text_again.read()
